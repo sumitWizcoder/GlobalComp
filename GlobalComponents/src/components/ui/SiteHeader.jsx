3cca5c9cc08887d55d5369096fa2533925241b28
sumitWizcoder <sumit.wizcoder@gmail.com>
2025-12-22 12:20:51 +0530

    remove transparent header add blur-md

--- GlobalComponents/src/components/ui/SiteHeader.jsx
+++ GlobalComponents/src/components/ui/SiteHeader.jsx
@@ -23,7 +23,7 @@ const SiteHeader = () => {
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${scrolled
         ? "bg-white/80 backdrop-blur-md py-5 shadow-sm"
-        : "bg-transparent py-5"
+        : "backdrop-blur-md py-5"
         }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
